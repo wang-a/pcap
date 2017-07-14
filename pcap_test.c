f5ee677c000a874740631c5d8852fcd16df499a2
root <root@localhost.localdomain>
2017-07-15 05:51:10 +0900

    commit fixed
    ip add

--- pcap_test.c
+++ pcap_test.c
@@ -3,35 +3,35 @@
 
 	 int main(int argc, char *argv[])
 	 {
-		pcap_t *handle;			/* Session handle */
-		char *dev;			/* The device to sniff on */
-		char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-		struct bpf_program fp;		/* The compiled filter */
-		char filter_exp[] = "port 23";	/* The filter expression */
-		bpf_u_int32 mask;		/* Our netmask */
-		bpf_u_int32 net;		/* Our IP */
-		struct pcap_pkthdr header;	/* The header that pcap gives us */
-		const u_char *packet;		/* The actual packet */
+		pcap_t *handle;			
+		char *dev;			
+		char errbuf[PCAP_ERRBUF_SIZE];	
+		struct bpf_program fp;		
+		char filter_exp[] = "port 80";	
+		bpf_u_int32 mask;		
+		bpf_u_int32 net;		
+		struct pcap_pkthdr header;	
+		const u_char *packet;		
 
-		/* Define the device */
+
 		dev = pcap_lookupdev(errbuf);
 		if (dev == NULL) {
 			fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
 			return(2);
 		}
-		/* Find the properties for the device */
+
 		if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
 			fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
 			net = 0;
 			mask = 0;
 		}
-		/* Open the session in promiscuous mode */
+
 		handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 		if (handle == NULL) {
 			fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
 			return(2);
 		}
-		/* Compile and apply the filter */
+
 		if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
 			fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
 			return(2);
@@ -40,11 +40,23 @@
 			fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
 			return(2);
 		}
-		/* Grab a packet */
-		packet = pcap_next(handle, &header);
-		/* Print its length */
-		printf("Jacked a packet with length of [%d]\n", header.len);
-		/* And close the session */
+		int status = 0;
+		while(1){
+			status= pcap_next_ex(handle, &header, &packet);
+			if(status == 0) {
+				continue;
+			}
+			
+			printf("destination Mac[%02x:%02x:%02x:%02x:%02x:%02x]\n", packet[0], packet[1], packet[2], packet[3], packet[4], packet[5]);
+			printf("source Mac [%02x:%02x:%02x:%02x:%02x:%02x]\n", packet[6], packet[7], packet[8], packet[9], packet[10], packet[11]);
+			puts("");
+			if(packet[12]==0x08 && packet[13]==0x00){
+				printf("IP:[%d.%d.%d.%d]\n",packet[26],packet[27],packet[28],packet[29]);
+			}
+			puts("");		
+		}
+		
+		
 		pcap_close(handle);
 		return(0);
 	 }
